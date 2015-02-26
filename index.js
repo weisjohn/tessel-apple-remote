c2a9d5847d038f9fcad34d00d6917f38f5fe92d6
John Weis <weis.john@gmail.com>
2015-02-26 00:05:40 -0500

    remove profiling

--- index.js
+++ index.js
@@ -161,8 +161,6 @@ module.exports = function(port) {
 
     ir.on('data', function(data) {
 
-        var start = Date.now();
-
         // don't do any processing if we're just getting interference
         if (data.length < 6) return;
 
@@ -186,8 +184,6 @@ module.exports = function(port) {
                 proxy_emit(id + "." + button);
             } 
         }
-
-        console.log("duration:", Date.now() - start);
 
     });
 
